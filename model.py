added a generator to deal with the large dataset also fixed a leakage in the validation set by splitting early and not using augmentation on the validation set

model.py
@@ -2,14 +2,23 @@ import pandas as pd
 import numpy as np
 import os
 import cv2
-from keras.models import Model
+import sklearn
+from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D
 import matplotlib.pyplot as plt
 
 DATA_DIR = '/opt/carnd_p3/data'
+VALIDATION_SIZE = 0.2
+BATCH_SIZE = 32
+
+# if using both, this will lead to memory error if not using a Generator/Dataloader
+# ATTENTION: flips will lead to leakage in validation score if splitting is not done right
+# splitting must be done before the augmentation
+# and also before adding left and right pictures to the mix
 USE_AUGMENTATION = True
-USE_LEFT_RIGHT = True
+USE_LEFT_RIGHT = False
+LEFT_RIGHT_ANGLE_CORRECTION = 0.06
 
 driving_log = pd.read_csv(os.path.join(DATA_DIR, "driving_log.csv"), skipinitialspace=True)
 # print(driving_log.head())
@@ -17,45 +26,59 @@ driving_log = pd.read_csv(os.path.join(DATA_DIR, "driving_log.csv"), skipinitial
 driving_log["center"] = '/opt/carnd_p3/data/' + driving_log["center"]
 driving_log["left"] = '/opt/carnd_p3/data/' + driving_log["left"]
 driving_log["right"] = '/opt/carnd_p3/data/' + driving_log["right"]
-print(driving_log.head())
+print("Driving Log head: ", driving_log.head())
 
-images = []
-for image in driving_log["center"]:
-    images.append(cv2.imread(image))
+train_driving_log, validation_driving_log = train_test_split(driving_log, test_size=VALIDATION_SIZE)
+print("Len train_driving_log: ", len(train_driving_log))
+print("Len validation_driving_log: ", len(validation_driving_log))
 
-if USE_LEFT_RIGHT:
-    for image in driving_log["left"]:
-        images.append(cv2.imread(image))
 
-    for image in driving_log["right"]:
-        images.append(cv2.imread(image))
+def generator(samples_df, batch_size=32, is_training=False):
+    num_samples = len(samples_df)
 
-images = np.array(images)
-print("Image shape: ", images.shape)
+    while 1:  # Loop forever so the generator never terminates
+        # if training: shuffle dataframe on epoch level, for validation set shuffling should not be used!
+        if is_training is True:
+            samples_df = samples_df.sample(frac=1)
 
-if USE_AUGMENTATION:
-    images_flipped = images[:, :, ::-1, :]
-    images = np.concatenate((images, images_flipped))
-    print("Image shape after adding flips: ", images.shape)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples_df[offset:offset + batch_size]
+            images = []
+            angles = []
+            for index, row in batch_samples.iterrows():
+                image_center = cv2.imread(row["center"])
+                images.append(image_center)
+                angles.append(row["steering"])
+                if (USE_AUGMENTATION is True) and (is_training is False):
+                    image_center_flipped = np.fliplr(image_center)
+                    images.append(image_center_flipped)
+                    angles.append(-row["steering"])
+                # Use the left and right pictures if in training set
+                if (USE_LEFT_RIGHT is True) and (is_training is False):
+                    image_left = cv2.imread(row["left"])
+                    image_right = cv2.imread(row["right"])
+                    images.append(image_left)
+                    angles.append(row["steering"] + LEFT_RIGHT_ANGLE_CORRECTION)
+                    images.append(image_right)
+                    angles.append(row["steering"] - LEFT_RIGHT_ANGLE_CORRECTION)
+                    if (USE_AUGMENTATION is True) and (is_training is False):
+                        image_left_flipped = np.fliplr(image_left)
+                        image_right_flipped = np.fliplr(image_right)
+                        images.append(image_left_flipped)
+                        angles.append(row["steering"] + LEFT_RIGHT_ANGLE_CORRECTION)
+                        images.append(image_right_flipped)
+                        angles.append(-(row["steering"] - LEFT_RIGHT_ANGLE_CORRECTION))
 
-X_train = np.array(images)
-y_train_center = np.array(driving_log["steering"].values)
+            X_train = np.array(images)
+            y_train = np.array(angles)
 
-if USE_LEFT_RIGHT is True:
-    correction = 0.1  # this is a parameter to tune
-    y_train_left = np.array(driving_log["steering"].values) + correction
-    y_train_right = np.array(driving_log["steering"].values) - correction
-    y_train = np.concatenate((y_train_center, y_train_left, y_train_right), axis=0)
-else:
-    y_train = y_train_center
+            # shuffle again on batch level (needed?)
+            yield sklearn.utils.shuffle(X_train, y_train)
 
-if USE_AUGMENTATION is True:
-    y_train_flipped = -y_train
-    y_train = np.concatenate((y_train, y_train_flipped))
 
-# print(y_train)
-# print(y_train.min())
-# print(y_train.max())
+# compile and train the model using the generator function
+train_generator = generator(train_driving_log, batch_size=BATCH_SIZE)
+validation_generator = generator(validation_driving_log, batch_size=BATCH_SIZE)
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
@@ -72,7 +95,11 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5, verbose=1)
+history_object = model.fit_generator(train_generator,
+                                     steps_per_epoch=np.ceil(len(train_driving_log) / BATCH_SIZE),
+                                     validation_data=validation_generator,
+                                     validation_steps=np.ceil(len(validation_driving_log) / BATCH_SIZE),
+                                     epochs=5, verbose=1)
 
 model.save("model.h5")
 